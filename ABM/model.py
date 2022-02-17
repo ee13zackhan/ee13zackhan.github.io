0b415fe82f5abc83f3d11d1871382642da7b1979
ee13zackhan <98338909+ee13zackhan@users.noreply.github.com>
2022-02-17 22:29:04 +0000

    Model animated with 2 stopping conditions
    can be stopped at a certain self.store level for all agents or after a certain number of iterations

--- ABM/model.py
+++ ABM/model.py
@@ -14,9 +14,10 @@ num_of_iterations = 10
 neighbourhood = 20
 agents = []
 colours = ["red","blue","green","yellow","purple","orange","white","black"]
+carry_on = True
 
-fig = matplotlib.pyplot.figure(figsize=(7, 7))
-ax = fig.add_axes([0, 0, 300, 300])
+fig = matplotlib.pyplot.figure(figsize=(5, 5))
+#ax = fig.add_axes([0, 0, 300, 300])
 
 random.seed(1)
 
@@ -26,28 +27,37 @@ for i in range(num_of_agents):
     
 #    print(f"Agent ID={agents[i].idnum}, y={agents[i].y}, x={agents[i].x}")
 
-
 # Move the agents, make them eat and make them share.
 def update(frame_number):   
     fig.clear()
-    # print(f"Frame Number = {frame_number}")
     random.shuffle(agents)
     matplotlib.pyplot.imshow(enviro.environment)
+    global carry_on
+    
     for i in range(num_of_agents):
         agents[i].move()
         agents[i].eat()
         agents[i].share_with_neighbours(neighbourhood)
-        #print(f"ID = {agents[i].idnum}")
+        # print(f"I am {agents[i].idnum}, my store is {agents[i].store}")
+        
         matplotlib.pyplot.xlim(0, 100)
         matplotlib.pyplot.ylim(0, 100)
-       # matplotlib.pyplot.imshow(enviro.environment)
         matplotlib.pyplot.scatter(agents[i].y,agents[i].x, color=agents[i].colour)
-        #print(agents[i].y,agents[i].x)
-    
         
+    stop = all(agents[i].store >= 100 for i in range(num_of_agents))
+    
+    if stop:
+        carry_on = False
+        
+def stopping():
+    a=0
+    while (a<10) & (carry_on):
+        # print(a)
+        yield a
+        a += 1
+    
 
-
-animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations)
+animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, frames=stopping ,repeat=False)
 matplotlib.pyplot.show()
 # matplotlib.pyplot.xlim(0, 300)
 # matplotlib.pyplot.ylim(0, 300)
